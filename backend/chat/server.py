b1d4714d8ca37824e2cdaeb5c8d4fdfcadee1c5f
Athy k <contact@athyk.me>
2025-02-21 00:22:40 +0000

    commenting

--- backend/chat/server.py
+++ backend/chat/server.py
@@ -2,7 +2,12 @@ import os
 import grpc
 from concurrent import futures
 
-from backend.common.proto import chat_pb2_grpc,chat_pb2
+# If paths are not resolved correctly, you may copy the files `chat_pb2.py`, `chat_pb2_grpc.py`, and `chat_pb2.pyi`
+# to this folder and replace the below import statement. Ensure those moved files are not committed to the repository.
+#
+# Alternatively you may convert the following for use on your operating system: PYTHONPATH=$(pwd)/backend/common/proto
+# But if any issues happen, use the docker compose command to run the server.
+from backend.common.proto import chat_pb2_grpc, chat_pb2
 
 
 class Service(chat_pb2_grpc.ChatServicer):
@@ -22,5 +27,6 @@ def serve():
     server.wait_for_termination()
 
 
-if __name__ == '__main__': # pragma: no cover
+# Below allows running the server directly using `python server.py` although not recommended.
+if __name__ == '__main__':  # pragma: no cover
     serve()
