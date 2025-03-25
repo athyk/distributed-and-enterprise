59a910c378cc03656d62e7a875b3ad1fd4735141
ZahinHussain <135178486+ZahinHussain@users.noreply.github.com>
2025-03-25 12:09:35 +0000

    Update +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import AnnouncementCard from '$components/announcementCard/announcementCard.svelte';
     import EventCard from '$components/eventCard/eventCard.svelte';
+    import { get } from '$lib/api/get';
+	import { onMount } from 'svelte';
 </script>
 
 <div class="min-h-screen p-4 sm:p-8 lg:p-12 flex flex-col items-center">
